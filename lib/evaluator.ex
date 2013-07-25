6a2e293e9d8a1d7eff3d26a9212a41726a15c60c
Gustavo Brunoro <brunoro@devio.us>
2013-07-25 20:02:41 -0300

    implemented basic multi-process debugging
    * anonymous functions and receive use the -> (match) operator.
    * spawn initializes another Evaluator server.
    * PIDs are being passed around as binaries, which kinda work for
      now, but can cause problems on the future.
    * coded the <- operator explictly, to deal with PIDs as binaries.
    * `self` still have to be specified, as for now it is getting the
      PID of the Evaluator process, not the debugged one.

--- lib/evaluator.ex
+++ lib/evaluator.ex
@@ -6,7 +6,7 @@ defmodule Evaluator do
   defmacro defdebug(header, do: body) do
     # TODO: binding retrieved via __CALLER__ had all variables as nil
     { _, _, quoted_params } = header
-    vars = Enum.map quoted_params, fn({ var, meta, module }) ->
+    vars = Enum.map quoted_params || [], fn({ var, meta, module }) ->
       { var, ({ var, meta, module }) }
     end
 
@@ -29,19 +29,38 @@ defmodule Evaluator do
     scope = :elixir_scope.vars_from_binding(:elixir_scope.to_erl_env(state.scope), state.binding)
     { :ok, state.scope(scope) }
   end
+    
+  # casts
+  def handle_cast(:done, state) do
+    { :stop, :normal, state }
+  end
   
+  def handle_cast(:pop_state, State[stack: [state]]), do: { :noreply, state }
+  def handle_cast(:pop_state, State[stack: [old_state | _]]) do
+    { :noreply, old_state }
+  end
+
+  def handle_cast(:push_state, state) do
+    { :noreply, state.stack([state | state.stack]) }
+  end
+
+  # calls
   def handle_call({ :eval, expr }, _from, state) do
     { value, new_state } = Evaluator.eval_quoted(expr, state)
     { :reply, value, new_state }
   end
- 
+
   def handle_call({ :expand, expr }, _from, state) do
     { _, meta, _ } = expr
     ex_scope = :elixir_scope.to_ex_env({ meta[:line] || 0, state.scope })
     expanded = Macro.expand_once(expr, ex_scope)
  
-    { :reply, expanded, state}
-    end
+    { :reply, expanded, state }
+  end
+
+  def handle_call(:get_state, _from, state) do
+    { :reply, state, state }
+  end
 
   def handle_call({ :match, { value, clauses }}, _from, state) do
     { matching, new_state } = 
@@ -50,17 +69,14 @@ defmodule Evaluator do
     # TODO: is this right? scope should be specific to match operation
     { :reply, matching, new_state }
   end
- 
-  def handle_cast(:done, state) do
-    { :stop, :normal, state }
-  end
-  
-  def handle_cast(:push_state, state) do
-    { :noreply, state.stack([state | state.stack]) }
-  end
-  
-  def handle_cast(:pop_state, State[stack: [old_state | _]]) do
-    { :noreply, old_state }
+
+  def handle_call(:receive, _from, state) do
+    receive_code = quote do
+      receive do
+        value -> value
+      end
+    end
+    Evaluator.eval_quoted(receive_code, state)
   end
 
   def find_matching_clause(value, clauses, state) do 
@@ -97,13 +113,15 @@ defmodule Evaluator do
   end
 
   # client functions
+  def done(pid),       do: :gen_server.cast(pid, :done)
+  def pop_state(pid),  do: :gen_server.cast(pid, :pop_state)
   def push_state(pid), do: :gen_server.cast(pid, :push_state)
-  def pop_state(pid), do: :gen_server.cast(pid, :pop_state)
-  def done(pid), do: :gen_server.cast(pid, :done)
 
-  def eval(pid, expr), do: :gen_server.call(pid, { :eval, expr })
-  def expand(pid, expr), do: :gen_server.call(pid, { :expand, expr })
+  def eval(pid, expr),            do: :gen_server.call(pid, { :eval, expr })
+  def expand(pid, expr),          do: :gen_server.call(pid, { :expand, expr })
+  def get_state(pid),             do: :gen_server.call(pid, :get_state)
   def match(pid, value, clauses), do: :gen_server.call(pid, { :match, { value, clauses }})
+  def receive_value(pid),         do: :gen_server.call(pid, :receive)
 
   # Makes nested Evaluator.next calls until leafs are reached.
   # Evaluates leaf expressions by sending them to pid, which
@@ -120,26 +138,68 @@ defmodule Evaluator do
     end
   end
 
+  # discard state changes made by evals ran by fun/0
+  defp do_on_new_state(pid, fun) do
+    # enter a new state level
+    Evaluator.push_state(pid)
+    return = fun.()
+    Evaluator.pop_state(pid)
+    return
+  end
+
   # values shouldn't be evaluated
   def next(_, value) when is_number(value), do: value
   def next(_, value) when is_binary(value), do: value
   def next(_, value) when is_atom(value),   do: value
 
-  # TODO: same format for case, receive, try
-  #       can case be redefined?
-  def next(pid, { :case, _, body }) do
-    [condition | [clauses]] = body
- 
+  # case
+  def next(pid, { :case, _, [condition | [do: clauses]] }) do
     condition_value = Evaluator.next(pid, condition)
-    Evaluator.match_next(pid, condition_value, clauses[:do]) # is there more than do?
+    Evaluator.match_next(pid, condition_value, clauses) # is there more than do?
   end
 
-  # On assignments only the right side is evaluated separately
+  # receive
+  def next(pid, { :receive, _, [[do: clauses]] }) do
+    received_value = Evaluator.receive_value(pid)
+    Evaluator.match_next(pid, received_value, clauses) 
+  end
+
+  # anonymous functions
+  def next(pid, { :fn, _, [[do: clauses]] }) do
+    received_value = Evaluator.receive_value(pid)
+    Evaluator.match_next(pid, received_value, clauses) 
+  end
+
+  # spawn should spawn another evaluator as well
+  def next(pid, { :spawn, _, [fun] }) do
+    state = Evaluator.get_state(pid)
+
+    proc_pid = spawn fn ->
+      { :ok, evaluator_pid } = :gen_server.start_link(Evaluator, state, [])
+      Evaluator.next(evaluator_pid, fun)
+      Evaluator.done(evaluator_pid)
+    end
+
+    # pids are stored as binaries (because of `invalid quoted expression: #PID<..>`)
+    pid_to_list(proc_pid)
+  end
+
+  # sending messages
+  # TODO: pids are being sent over the as binaries, which is clearly not a good idea
+  def next(pid, { :<-, _, [esc_dest_pid, msg] }) do
+    msg_val  = Evaluator.next(pid, msg)
+    dest_pid = list_to_pid(Evaluator.next(pid, esc_dest_pid))
+
+    dest_pid <- msg_val
+  end
+
+  # assignments
   def next(pid, { :=, meta, [left | [right]] }) do
     right_value = Evaluator.next(pid, right)
     Evaluator.eval(pid, { :=, meta, [left | [right_value]] })
   end
 
+  # list of expressions
   def next(pid, { type, meta, expr_list }) when is_list(expr_list) do
     expr = { type, meta, expr_list }
 
@@ -149,6 +209,7 @@ defmodule Evaluator do
     end
   end
 
+  # other expressions are evaluated directly
   def next(pid, expr) do
     do_or_expand pid, expr, fn ->
       Evaluator.eval(pid, expr)
@@ -158,16 +219,10 @@ defmodule Evaluator do
   # pattern matching operator should evaluate clauses until
   # the first clause matching the condition is found
   def match_next(pid, value, { :-> , _, clauses }) do
-    # enter a new state level
-    Evaluator.push_state(pid)
-    
-    matching_clause = Evaluator.match(pid, value, clauses)
-    { _, _, right } = matching_clause
-    next_value = Evaluator.next(pid, right)
-    
-    # go back to the previous state level
-    Evaluator.pop_state(pid)
-
-    next_value
+    do_on_new_state pid, fn ->
+      matching_clause = Evaluator.match(pid, value, clauses)
+      { _, _, right } = matching_clause
+      Evaluator.next(pid, right)
+    end
   end
 end
