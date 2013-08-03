512c27f2dde33aa9fd7649c35c20aec5c05aed3d
Gustavo Brunoro <brunoro@devio.us>
2013-08-03 16:03:45 -0300

    better coordinator testing

--- test/coordinator_test.exs
+++ test/coordinator_test.exs
@@ -4,6 +4,24 @@ defmodule CoordinatorTest do
   use ExUnit.Case
   alias Debugger.Coordinator
 
+  test "put/get state" do
+    binding = [a: 1]
+    scope = :elixir_scope.to_erl_env(__ENV__)
+    { :ok, coord } = Coordinator.start_link(binding, scope)
+
+    state = Coordinator.get_state(coord)
+    same_scope = :elixir_scope.vars_from_binding(scope, binding)
+    same_state = Coordinator.State[binding: binding, scope: same_scope]
+    assert state == same_state
+
+    new_binding = [a: 1, b: 2]
+    new_scope = :elixir_scope.vars_from_binding(scope, new_binding)
+    new_state = state.binding(new_binding).scope(new_scope)
+    Coordinator.put_state(coord, new_state)
+
+    assert new_state == Coordinator.get_state(coord)
+  end
+
   test "push/pop state stack" do
     binding = [a: 1]
     scope = :elixir_scope.to_erl_env(__ENV__)
@@ -26,5 +44,9 @@ defmodule CoordinatorTest do
 
     assert state == old_state
     assert old_state.binding[:b] == nil
+    
+    # can't pop more the stack
+    Coordinator.pop_stack(coord)
+    assert old_state == Coordinator.get_state(coord)
   end
 end
