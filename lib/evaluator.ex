99df27b1ba578821e0b7880f50dfe6ef70c23e69
Gustavo Brunoro <brunoro@devio.us>
2013-08-07 23:11:52 -0300

    Evaluator.eval_quoted now accepts temp_vars
    * optional argument, defaults to []
    * temp_vars are added to the binding before eval and removed from
      the binding after the evaluation
    * in case of name collisions, variables previously declared prevail
    * untreated corner case: names defined withing block of code and
      added as temp_vars aren't treated
    * used for evaluating exceptions, as they can't be added to the
      syntax tree.
    # Please enter the commit message for your changes. Lines starting
    # with '#' will be ignored, and an empty message aborts the commit.
    # On branch master
    # Your branch is ahead of 'origin/master' by 1 commit.
    #
    # Changes to be committed:
    #   (use "git reset HEAD <file>..." to unstage)
    #
    #	modified:   lib/evaluator.ex
    #
    # Untracked files:
    #   (use "git add <file>..." to include in what will be committed)
    #
    #	.temp_commit
    #	Elixir.Debugger.Coordinator.State.beam
    #	Elixir.Debugger.Coordinator.beam
    #	Elixir.Debugger.PIDName.beam
    #	evaluator.old.exs
    #	exception.ex
    #	lib/foo.exs

--- lib/evaluator.ex
+++ lib/evaluator.ex
@@ -3,10 +3,22 @@ defmodule Debugger.Evaluator do
 
   # how to evaluate expressions 
   # TODO: use scope on eval?
-  def eval_quoted(expr, state) do
+  def eval_quoted(expr, state, temp_vars//[]) do
     try do
-      { value, binding, scope } = :elixir.eval_quoted([expr], state.binding)
-      { clean_value, new_state } = wrap_pid(value, state.binding(binding).scope(scope))
+      # state.binding prevails
+      good_temp_vars = Enum.filter temp_vars, fn({ k, _ }) -> 
+        state.binding[k] == nil 
+      end
+      eval_binding = Keyword.merge good_temp_vars, state.binding 
+
+      { value, binding, scope } = :elixir.eval_quoted([expr], eval_binding)
+
+      new_binding = Enum.reduce good_temp_vars, binding, fn({ k, _ }, acc) ->
+        Keyword.delete acc, k
+      end
+      new_scope = :elixir_scope.vars_from_binding(state.scope, new_binding)
+
+      { clean_value, new_state } = wrap_pid(value, state.binding(new_binding).scope(new_scope))
       { :ok, clean_value, new_state }
     catch
       exception -> { :catch, exception }
@@ -84,15 +96,16 @@ defmodule Debugger.Evaluator do
     end
 
     clause_list = { :->, [], clause_list }
-    match_clause_case = quote do
+    exception_var = { :__EXCEPTION__, [], nil }
+    match_clause_try = quote do
       try do
-        raise unquote(exception)
+        raise unquote(exception_var)
       rescue
         unquote(clause_list)
       end
     end
 
-    eval_quoted(match_clause_case, state)
+    eval_quoted(match_clause_try, state, [__EXCEPTION__: exception])
   end
 
   def initialize_clause_vars(clauses, state) do
