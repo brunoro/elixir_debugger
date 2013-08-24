1f6e7616726cd3b810e9ce68a321944755934d93
Gustavo Brunoro <brunoro@devio.us>
2013-08-24 20:00:46 -0300

    simple untested step CLI

--- lib/debugger/cli.ex
+++ lib/debugger/cli.ex
@@ -11,23 +11,27 @@ defmodule Debugger.CLI do
     { :ok, nil }
   end
 
-  def handle_cast({ :next, pid, expr }, nil) do
-    IO.puts Macro.to_string expr
+  def handle_cast({ :next, pid, expr }, state) do
+    IO.puts "#{inspect pid}:\n#{Macro.to_string expr}"
     read_command(pid)
 
-    { :noreply, nil }
+    { :noreply, state }
+  end
+
+  def prompt(pid) do
+    IO.gets("debugger(#{inspect pid}) ") |> String.strip
   end
 
   def read_command(pid) do
-    case IO.gets "debug(#{inspect pid})>" do
+    case prompt(pid) do
       "step" ->
         step(pid)
-      _other ->
+      other ->
         read_command(pid)
     end
   end
 
-  def next(pid, expr), do: :gen_server.handle_cast(@server_name, { :next, pid, expr })
+  def next(pid, expr), do: :gen_server.cast(@server_name, { :next, pid, expr })
   def step(pid), do: pid <- :go
 
 end
